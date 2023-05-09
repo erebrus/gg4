f8cffc820a5913a7f8ae852b770aa67058dbb722
Ángela <angela.lbsr@gmail.com>
2023-05-09 18:53:34 +0200

    use deck in globals

--- src/gui/gui.gd
+++ src/gui/gui.gd
@@ -15,6 +15,8 @@ func _ready() -> void:
 	hand.piece_discarded.connect(_on_piece_placed)
 	hand.piece_placed.connect(_on_piece_placed)
 	
+	deck.pieces = Globals.deck
+	
 	if auto_draw_piece_on_place:
 		while hand.num_pieces < hand.max_pieces:
 			draw_from(deck)
