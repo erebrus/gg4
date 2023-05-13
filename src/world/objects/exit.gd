29b85a04766cb869f786301deb236511ddb7cc6a
erebrus <erebrus@gmail.com>
2023-05-13 20:33:38 +0200

    fixed exit

--- src/world/objects/exit.gd
+++ src/world/objects/exit.gd
@@ -1,16 +1,9 @@
 extends Area2D
 
-func _ready():
-	Events.turn_complete.connect(_on_turn_complete)
 
-func _on_turn_complete():
-	monitoring = true	
-	await get_tree().create_timer(.1).timeout
-	monitoring = false
-	
 func _on_body_entered(body):
-	monitoring = false
 	if body.commands.is_empty():	
 		body.tick_suspended=true
 		Globals.level_manager.level_complete=true
+		await get_tree().create_timer(1).timeout		
 		Events.level_complete.emit()
