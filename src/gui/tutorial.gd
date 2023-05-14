287e6c9846923af16a18893eec4573c30fb02b67
erebrus <erebrus@gmail.com>
2023-05-14 18:57:55 +0200

    tutorial level 1

--- src/gui/tutorial.gd
+++ src/gui/tutorial.gd
@@ -3,7 +3,7 @@ extends PanelContainer
 var messages:Array[String] =[
 	"Place movement tokens to reach the end tile. Don't overshoot it.",
 	"Don't forget you can rotate tokens.",	
-	"You can walk on grass and sand files.\nMountain tiles are impassable and will force you to discard a token if you try.",
+	"You can walk on grass and sand Tiles.\nMountain tiles are impassable and will force you to discard a token if you try.",
 	"If you collect a dice, you can get a surprise. Maybe good, maybe bad.",
 	"Your success will be measured by how light your discard bag is. Try to reach the end using the least possible number of tokens. Remember: Less is more. Less is quest.",
 	"See those cute creatures. They don't like you.\nIf you collide with them, you will discard your hand.",
--- src/level_manager.gd
+++ src/level_manager.gd
@@ -1,6 +1,9 @@
 class_name LevelManager extends Node
 
 var levels:Array[PackedScene] = [
+#	preload("res://src/world/levels/test_level_a.tscn"),
+	
+	preload("res://src/world/levels/tutorial_level_1.tscn"),
 	preload("res://src/world/levels/level_1.tscn"),
 	preload("res://src/world/levels/level_2.tscn"),
 	
