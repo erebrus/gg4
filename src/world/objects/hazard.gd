ad8911f673f77caaba8052c8f465846606efe08f
erebrus <erebrus@gmail.com>
2023-05-14 20:45:12 +0200

    fixed hazzard double trigger

--- src/world/objects/hazard.gd
+++ src/world/objects/hazard.gd
@@ -1,4 +1,14 @@
 extends Area2D
 
+var turns_from_trigger:=10
+func _ready():
+	Events.turn_complete.connect(_on_turn_complete)
+	
+func _on_turn_complete():
+	turns_from_trigger+=1
+		
 func _on_body_entered(body):
-	body.take_damage(1)
+	
+	if turns_from_trigger>1:
+		body.take_damage(1)
+		turns_from_trigger = 0
