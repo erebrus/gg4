e6ec679ccd0dcdd0ff8e9c0a17902ed5d422ec33
Ángela <angela.lbsr@gmail.com>
2023-05-14 20:11:30 +0200

    win sounds

--- src/win/win_screen.gd
+++ src/win/win_screen.gd
@@ -40,14 +40,14 @@ func animate_scale():
 	discard_pile.global_position.x = balance.right_plate_base.x
 	
 	tween.tween_property(discard_pile, "position", balance.right_plate_base, 0.3).set_ease(Tween.EASE_IN)
-	tween.tween_callback(balance.add_to_right_plate.bind(discard_pile))
+	tween.tween_callback(add_discard_pile_to_plate)
+	
 	tween.tween_property(balance, "angle", 0.5, 0.2)
 	tween.tween_property(balance, "angle", 0.3, 0.1)
 	tween.tween_property(balance, "angle", 0.45, 0.1)
 	tween.tween_property(balance, "angle", 0.4, 0.1)
 	await tween.finished
 	
-	Globals.level_manager.last_discard_size = 35
 	var final_rank = RANKS.keys().back()
 	
 	var min_angle = -0.1
@@ -66,14 +66,23 @@ func animate_scale():
 	
 	tween = create_tween()
 	tween.tween_property(deck_pile, "position", balance.left_plate_base, 0.3).set_ease(Tween.EASE_IN)
-	tween.tween_callback(balance.add_to_left_plate.bind(deck_pile))
+	tween.tween_callback(add_deck_pile_to_plate)
 	tween.tween_property(balance, "angle", target_angle - 0.1, 0.2)
 	tween.tween_property(balance, "angle", target_angle + 0.1, 0.1)
 	tween.tween_property(balance, "angle", target_angle - 0.05, 0.1)
 	tween.tween_property(balance, "angle", target_angle, 0.1)
 	tween.tween_interval(0.2)
-	tween.tween_callback($TitlePanel.show)
 	
-	tween.finished
+	await tween.finished
+	
+	$rank.play()
+	$TitlePanel.show()
 	
 
+func add_discard_pile_to_plate():
+	$squeak.play()
+	balance.add_to_right_plate(discard_pile)
+
+func add_deck_pile_to_plate():
+	$squeak.play()
+	balance.add_to_left_plate(deck_pile)
